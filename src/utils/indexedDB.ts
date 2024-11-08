630cbb5dec90864abe73499524faf46d8737ce46
root <root@DESKTOP-R6SKAQ5.localdomain>
2024-11-08 17:30:51 +0800

    feat: add indexedDB add delete update get

--- src/utils/indexedDB.ts
+++ src/utils/indexedDB.ts
@@ -2,30 +2,110 @@
 
 export default class DB {
     private dbName: string//数据库名称
+    private db: any//数据库对象
     constructor(dbName: string) {
         this.dbName = dbName
     }
 
     //打开数据库
-    public openStore(storeName: string, keyPath: string) {
+    public openStore(storeName: string, keyPath: string, indexs?: Array<string>) {
         const request = window.indexedDB.open(this.dbName, 1)
 
-        request.onsuccess = (event) => {
+        request.onsuccess = (event: any) => {
+            this.db = event.target.result
             console.log('数据库打开成功', event)
         }
+
+
         request.onerror = (error) => {
             console.log('数据库打开失败', error)
         }
+
+
         request.onupgradeneeded = (event) => {
             console.log('数据库升级成功', event)
             const { result }: any = event.target
+
+            //建表
             const store = result.createObjectStore(storeName, {
                 autoIncrement: true,
                 keyPath
             })
+
+            //添加字段
+            indexs?.map((v: string) => {
+                store.createIndex(v, v, { unque: false })
+            })
+
             store.transaction.oncomplete = (e: any) => {
                 console.log('创建对象仓库里成功', e)
             }
+        }
+    }
+
+
+
+
+    //数据库的增删改查
+
+    //新增/修改数据库数据
+    updateItem(storeName: string, data: any) {
+        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const request = store.put({
+            ...data,
+            updateTime: new Date().getTime()
+        })
+        request.onsuccess = (event: any) => {
+            console.log('数据写入成功', event)
+        }
+
+
+        request.onerror = (error: any) => {
+            console.log('数据写入失败', error)
+        }
+    }
+
+    //删除数据库数据
+    deleteItem(storeName: string, key: number | string) {
+        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const request = store.delete(key)
+        request.onsuccess = (event: any) => {
+            console.log('删除数据写入成功', event)
+        }
+
+
+        request.onerror = (error: any) => {
+            console.log('删除数据写入失败', error)
+        }
+    }
+
+    //查询数据库数据
+    getList(storeName: string) {
+        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const request = store.getAll()
+        request.onsuccess = (event: any) => {
+            console.log('查询数据成功', event)
+            return event.target.result
+        }
+
+
+        request.onerror = (error: any) => {
+            console.log('查询数据失败', error)
+        }
+    }
+
+     //查询某一条数据库数据
+     getItem(storeName: string,key:string|number) {
+        const store = this.db.transaction([storeName], 'readwrite').objectStore(storeName)
+        const request = store.get(key)
+        request.onsuccess = (event: any) => {
+            console.log('查询某一条数据成功', event)
+            return event.target.result
+        }
+
+
+        request.onerror = (error: any) => {
+            console.log('查询某一条数据失败', error)
         }
     }
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,7 +8,40 @@ import IndexedDB from '@/utils/indexedDB'
 
 //模拟数据，indexedDB数据库
 const airbnbDB = new IndexedDB('airbnb')
-airbnbDB.openStore('room', 'id')
+airbnbDB.openStore('elephant', 'id', ['nose', 'ear'])
+
+//新增
+const addDb = () => {
+  airbnbDB.updateItem('elephant', {
+    nose: '33',
+    ear: 'big'
+  })
+}
+
+//修改数据库数据
+const editDb = () => {
+  airbnbDB.updateItem('elephant', {
+    id:1,
+    nose: '88',
+    ear: 'bigest'
+  })
+}
+//删除数据库数据
+const deleteDb = () => {
+  airbnbDB.deleteItem('elephant',1)
+}
+//查询所有数据库数据
+const getDb = () => {
+  airbnbDB.getList('elephant')
+  console.log(airbnbDB.getList('elephant'))
+}
+//查询所有数据库数据
+const getItem = async(key:number|string) => {
+ const res= await airbnbDB.getItem('elephant',key)
+  console.log('ddddddd',res)
+}
+
+
 const { t } = useI18n()
 
 console.log(useI18n)
@@ -36,7 +69,12 @@ getRoomListApi()
   <div class="home">
     <div class="box">{{ t('message.home') }} </div>
     <el-button :plain="true" @click="openVn">Show message</el-button>
-    <el-button>Default</el-button>
+    <el-button @click="addDb">add DB</el-button>
+    <el-button @click="editDb">edit DB</el-button>
+    <el-button @click="deleteDb">delete DB</el-button>
+    <el-button @click="getDb">getDb DB</el-button>
+    <el-button @click="getItem(2)">getItem DB</el-button>
+    
     <el-button type="primary">Primary</el-button>
     <el-button type="success">Success</el-button>
     <el-button type="info">Info</el-button>
